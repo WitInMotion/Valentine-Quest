cf79c96be819c84b36c87ed8587a3afd466e38ee
Habiba Musa <habibamusazee@gmail.com>
2026-02-13 15:43:54 +0100

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,45 @@
+
+import { Question } from './types';
+
+export const QUESTIONS: Question[] = [
+  {
+    id: 1,
+    text: "Do you believe in love at first swipe? (Or first sight?)",
+    image: "https://picsum.photos/seed/love1/400/300"
+  },
+  {
+    id: 2,
+    text: "Are you ready for some extremely high levels of affection?",
+    image: "https://picsum.photos/seed/love2/400/300"
+  },
+  {
+    id: 3,
+    text: "Would you agree that we make a pretty decent team?",
+    image: "https://picsum.photos/seed/love3/400/300"
+  },
+  {
+    id: 4,
+    text: "Can I borrow your heart for a while? (I promise to return it with interest!)",
+    image: "https://picsum.photos/seed/love4/400/300"
+  },
+  {
+    id: 5,
+    text: "Will you be my Valentine?",
+    image: "https://picsum.photos/seed/love5/400/300"
+  }
+];
+
+export const NO_MESSAGES: string[] = [
+  "Ouch! My heart just did a backflip into a puddle. 💔",
+  "Wait, let's pretend that was a misclick... right? 🤨",
+  "Think about the poor kittens who would be sad! 🐱",
+  "I'm going to tell my mom you said that! 😭",
+  "Are you sure? I've got snacks! 🍫",
+  "That button is broken, try the green one! 🛠️",
+  "System Error: Adorable user found rejecting love. Please retry. 🤖",
+  "I'll eat all the pizza by myself then... 🍕",
+  "You're breaking the internet with that answer! 🌐",
+  "Please? I'll do the dishes for a week! 🧼",
+  "Wait! Look at this distraction! *points nowhere* 💨",
+  "I'm not crying, it's just eye-sweat. 👁️💧"
+];
